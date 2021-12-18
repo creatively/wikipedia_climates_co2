751e0b424e9f9115cf6bbb5cc3400c66d0492682
Chris Charles <chris@creatively.co.uk>
2021-12-18 17:43:04 +0000

    Generating  totalHeatingAndCoolingScore + refactor + bug spot

--- code_utilities.js
+++ code_utilities.js
@@ -1,3 +1,4 @@
 const el = (selector, parentElement) => { return parentElement ? parentElement.querySelector(selector) : document.querySelector(selector) };
 const els = (selector, parentElement) => { return parentElement ? parentElement.querySelectorAll(selector) : document.querySelectorAll(selector) };
 const elsArray = (selector, parentElement) => { return parentElement ? Array.from(parentElement.querySelectorAll(selector)) : Array.from(document.querySelectorAll(selector)) };
+const arrayTotal = arr => { let total = 0; arr.forEach(item => {total += Number(item)}); return total}
--- main.js
+++ main.js
@@ -15,9 +15,9 @@ getArrayOfMonthlyMeanTemps = () => {
     if (meanTempsHeading) {
         const rowOfMonthlyMeanTemps = meanTempsHeading.parentElement;
 
-        const isFareignheight = elsArray('th', table).filter(
+        const isFareignheight = elsArray('th', table).some(
             rowTh => rowTh.textContent.includes('mean °F')
-        ).length > 0; 
+        ); 
         const monthlyMeanColumns = elsArray('td', rowOfMonthlyMeanTemps);
 
         const convertFtoC = (f) => ((f - 32) * 5 / 9).toFixed(1);
@@ -42,6 +42,26 @@ getArrayOfMonthlyMeanTemps = () => {
     }
 }
 
-log('getArrayOfMonthlyMeanTemps ...');
-log(getArrayOfMonthlyMeanTemps());
+const arrayOfMonthlyMeanTemps = getArrayOfMonthlyMeanTemps();
 
+if (arrayOfMonthlyMeanTemps) {
+    const arrayTotal = arr => { let total = 0; arr.forEach(item => {total += Number(item)}); return total}
+
+    const optimumMeanTemp = 17;
+
+    const totalHeatingAndCoolingScore = arrayTotal(
+        arrayOfMonthlyMeanTemps.map(
+            temp => Math.abs(optimumMeanTemp - temp)
+        )
+    ).toFixed(1);
+
+    console.log('totalHeatingAndCoolingScore = ', totalHeatingAndCoolingScore);
+} else {
+    // no 'mean' row in table
+}
+
+
+
+// NEXT : 
+// <<--- bug reoccured for months below 0F being 'NaN'
+// --->> create carbon emissions factor to add in
